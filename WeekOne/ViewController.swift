bb22c7764e75d4a26f519f2f0fafe0abb55576cd
Benjamin Tsai <benjamintsa@gmail.com>
2020-01-15 14:19:17 -0500

    Lesson 2.4

--- WeekOne/ViewController.swift
+++ WeekOne/ViewController.swift
@@ -16,9 +16,37 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    var count1 = 0
     @IBAction func sayItPressed(_ sender: UIButton) {
-        messageLabel.text = "Swiftacular"
+        if count1%2 == 0 {
+            messageLabel.text = "Swiftacular"
+            messageLabel.textColor = UIColor.yellow
+            count1+=1
+        }
+            
+        else{
+            messageLabel.text = "Makin' Apps Swiftly"
+            messageLabel.textColor = UIColor.cyan
+            count1-=1
+        }
     }
+    var count2 = 0
+    @IBAction func showOtherPressed(_ sender: UIButton) {
+        if count2%2 == 0 {
+            messageLabel.text = "You are Awesome"
+            messageLabel.textColor = UIColor.blue
+            count2+=1
+        }
+            
+        else{
+            messageLabel.text = "Swift is Awesome"
+            messageLabel.textColor = UIColor.black
+            count2-=1
+        }
+
+        
+    }
+    
     
 
 }
