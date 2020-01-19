ff0bf49b776c6be8173a6c4715a4846e336d45af
Benjamin Tsai <benjamintsa@gmail.com>
2020-01-19 15:06:27 -0500

    Lesson 2.8

--- WeekOne/ViewController.swift
+++ WeekOne/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var myMessage: UILabel!
     @IBOutlet weak var myImage: UIImageView!
     
+    var imageNumber = 0
+    
     let swiftMessage = "Swiftacular"
     let coolMessage = "You Are Cool"
     let makinMessage = "Makin' Apps Swiftly"
@@ -21,31 +23,42 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         myMessage.text = ""
+        myImage.contentMode = .scaleAspectFit
         // Do any additional setup after loading the view.
     }
     @IBAction func showMessagePressed(_ sender: UIButton) {
-        if  myMessage.text == swiftMessage{
-            myMessage.text = makinMessage
-            myMessage.textColor = UIColor.yellow
-            myMessage.textAlignment = NSTextAlignment.center
-            myImage.image = UIImage(named: "image0")
-            
+        
+        //let imageName = "image" + String(imageNumber)
+        let imageName = "image\(imageNumber)"
+        myImage.image = UIImage(named: imageName)
+        imageNumber += 1
+        if imageNumber == 10{
+            imageNumber = 0
         }
-        else if myMessage.text == coolMessage || myMessage.text == "" {
-            myMessage.text = swiftMessage
-            myMessage.textColor = UIColor.red
-            myMessage.textAlignment = NSTextAlignment.left
-            myImage.image = UIImage(named: "image2")
-            
-        }
-            
-        else{
-            myMessage.text = coolMessage
-            myMessage.textColor = UIColor.cyan
-            myMessage.textAlignment = NSTextAlignment.right
-            myImage.image = UIImage(named: "image1")
-            
-        }
+        
+         
+        //        if  myMessage.text == swiftMessage{
+        //            myMessage.text = makinMessage
+        //            myMessage.textColor = UIColor.yellow
+        //            myMessage.textAlignment = NSTextAlignment.center
+        //            myImage.image = UIImage(named: "image0")
+        //
+        //        }
+        //        else if myMessage.text == coolMessage || myMessage.text == "" {
+        //            myMessage.text = swiftMessage
+        //            myMessage.textColor = UIColor.red
+        //            myMessage.textAlignment = NSTextAlignment.left
+        //            myImage.image = UIImage(named: "image2")
+        //
+        //        }
+        //
+        //        else{
+        //            myMessage.text = coolMessage
+        //            myMessage.textColor = UIColor.cyan
+        //            myMessage.textAlignment = NSTextAlignment.right
+        //            myImage.image = UIImage(named: "image1")
+        //
+        //        }
     }
     
     
